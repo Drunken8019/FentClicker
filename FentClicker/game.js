684c14b78609612a0eff6be73f0c1c8ff1589c06
Drunken8019 <hebesbergera@gmail.com>
2025-01-31 11:01:11 +0100

    hhh

--- FentClicker/game.js
+++ FentClicker/game.js
@@ -1,15 +1,27 @@
 var FentClick = document.getElementById("fentClick");
 var FentAdded = document.getElementById("fentAdded");
 var currentFent = document.getElementById("currentFent");
+var fentNeedleBuy = document.getElementById("fentNeedleBuy");
+
 var Fent = 0;
+var linearIncr = 0;
+
 FentClick.addEventListener("click", function() {
-    increaseFent(1);
+    increaseFent(linearIncr);
+});
+
+fentNeedleBuy.addEventListener("click", function() {
+    if(Fent >= 50)
+        {
+            Fent -= 50;
+            linearIncr++;
+        }
 });
 
 function increaseFent(mult)
 {
-    Fent++;
-    FentAdded.innerHTML = 1 * mult;
+    Fent += 1 + mult;
+    FentAdded.innerHTML = 1 + mult;
     FentAdded.style.opacity = "100%";
     FentAdded.style.left = (event.clientX + 5) + "px";
     FentAdded.style.top = (event.clientY + 15) + "px";
