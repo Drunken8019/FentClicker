0296290f20282c0783c6a98d11ae4452238fce47
Drunken8019 <hebesbergera@gmail.com>
2025-01-31 10:46:33 +0100

    calcTS

--- FentClicker/game.js
+++ FentClicker/game.js
@@ -1,15 +1,18 @@
 var FentClick = document.getElementById("fentClick");
 var FentAdded = document.getElementById("fentAdded");
+var currentFent = document.getElementById("currentFent");
 var Fent = 0;
-FentClick.addEventListener("click", increaseFent);
+FentClick.addEventListener("click", function() {
+    increaseFent(1);
+});
 
-function increaseFent()
+function increaseFent(mult)
 {
     Fent++;
-    FentAdded.innerHTML = Fent;
+    FentAdded.innerHTML = 1 * mult;
     FentAdded.style.opacity = "100%";
     FentAdded.style.left = (event.clientX + 5) + "px";
     FentAdded.style.top = (event.clientY + 15) + "px";
     setTimeout(() => {FentAdded.style.opacity = "0";}, 500)
-    
+    currentFent.innerHTML = "FentStash: " + Fent + "g";
 }
