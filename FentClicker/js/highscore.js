5d9cc14c3b9270191be9d72897630b1322b2dc56
haiderer_p <haidererpaul@gmail.com>
2025-02-01 13:32:43 +0100

    highscore anfang (musss noch gefixed werden)

---
+++ FentClicker/js/highscore.js
@@ -0,0 +1,24 @@
+/*let fentCount = 0;
+let fentPerClick = 1;
+let highscore = localStorage.getItem("highscore") || 0;
+
+const fentDisplay = document.getElementById("currentFent");
+const fentClickButton = document.getElementById("fentClick");
+const highscoreDisplay = document.getElementById("highscore");
+highscoreDisplay.innerText = `Highscore: ${highscore}`;
+
+document.body.insertBefore(highscoreDisplay, fentDisplay.nextSibling);
+
+fentClickButton.addEventListener("click", () => {
+    fentCount += fentPerClick;
+    fentDisplay.innerText = `FentStash: ${fentCount}`;
+
+    if (fentCount > highscore) {
+        highscore = fentCount;
+        localStorage.setItem("highscore", highscore);
+        highscoreDisplay.innerText = `Highscore: ${highscore}`;
+    }
+});
+
+geht nu nd zählt immer mit 1 egal ob upgrade
+*/
