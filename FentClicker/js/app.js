32d20194bd21f62941937b602d550d94b3582556
Drunken8019 <hebesbergera@gmail.com>
2025-09-03 11:43:43 +0200

    lotta shii

--- FentClicker/js/app.js
+++ FentClicker/js/app.js
@@ -1,23 +1,61 @@
 var FentClick = document.getElementById("fentClick");
 var FentAdded = document.getElementById("fentAdded");
 var currentFent = document.getElementById("currentFent");
+var fentPerClick = document.getElementById("fentPerClick");
+var autoFarmAmount = document.getElementById("autoFarm");
 var fentNeedleBuy = document.getElementById("fentNeedleBuy");
 var fentPipeBuy = document.getElementById("fentPipeBuy");
+var fentReactorBuy = document.getElementById("fentReactorBuy");
+var jewishFloydBuy = document.getElementById("jewishFloydBuy");
+var crackBuy = document.getElementById("crackBuy");
+var boostContainer = document.getElementById("boostContainer");
+var dealer = document.getElementById("dealer");
 
-var linearIncr = 0;
-var expIncr = 1;
-var Fent = 0;
+var click = 1;
+var clickMult = 1;
+var Fent = 9999999;
+var autoFarm = 0;
+var autoFarmMult = 1;
+var globalClickMult = 1;
 
-var fentNeedle = new FentNeedle;
-var fentPipe = new FentPipe;
+class GameHandler
+{
+    static setPrice(price, elem)
+    {
+        elem.innerHTML = "Price: " + price + " Fent";
+    }
 
+    static clickValue()
+    {
+        return click * clickMult * globalClickMult;
+    }
+
+    static farmValue()
+    {
+        return autoFarm * autoFarmMult;
+    }
+
+    static increaseFent()
+    {
+        Fent += this.clickValue();
+        this.displayPopUp(this.clickValue());
+    }
+
+    static displayPopUp(content)
+    {
+        FentAdded.innerHTML = content;
+        FentAdded.style.opacity = "100%";
+        FentAdded.style.left = (event.clientX + 5) + "px";
+        FentAdded.style.top = (event.clientY + 15) + "px";
+        setTimeout(() => {FentAdded.style.opacity = "0";}, 500)
+    }
+}
 
 class FentNeedle
 {
     constructor()
     {
         this.price = 50;
-
     }
 
     buy()
@@ -25,9 +63,8 @@ class FentNeedle
         if(Fent >= this.price)
         {
             Fent -= this.price;
-            linearIncr++;
+            click++;
         }
-        update(); 
     }
 }
 
@@ -43,33 +80,58 @@ class FentPipe
         if(Fent >= this.price)
         {
             Fent -= this.price;
-            expIncr *= 2;
+            if(clickMult == 1) clickMult = 2;
+            else clickMult += 2;
+            this.price *= 10;
         }
-        update(); 
+        GameHandler.setPrice(this.price, document.getElementById("fentPipeBuyPrice"));
     }
 }
 
-class GameHandler
+class FentReactor
 {
-    static increaseFent(mult)
+    constructor()
     {
-        Fent += linearIncr + expIncr;
-        this.displayPopUp(linearIncr + expIncr);
-        update();
+        this.price = 5000;
     }
 
-    static displayPopUp(content)
+    buy()
     {
-        FentAdded.innerHTML = content;
-        FentAdded.style.opacity = "100%";
-        FentAdded.style.left = (event.clientX + 5) + "px";
-        FentAdded.style.top = (event.clientY + 15) + "px";
-        setTimeout(() => {FentAdded.style.opacity = "0";}, 500)
+        if(Fent >= this.price)
+        {
+            Fent -= this.price;
+            autoFarm += 100;
+        }
     }
 }
+
+class JewishFloyd
+{
+    constructor()
+    {
+        this.price = 1000000;
+    }
+
+    buy()
+    {
+        if(Fent >= this.price)
+        {
+            Fent -= this.price;
+            if(autoFarmMult == 1) autoFarmMult = 2;
+            else autoFarmMult += 2;
+            this.price *= 10;
+        }
+        GameHandler.setPrice(this.price, document.getElementById("jewishFloydBuyPrice"));
+    }
+}
+
+var fentNeedle = new FentNeedle;
+var fentPipe = new FentPipe;
+var fentReactor = new FentReactor;
+var jewishFloyd = new JewishFloyd;
 
 FentClick.addEventListener("click", function() {
-    GameHandler.increaseFent(linearIncr);
+    GameHandler.increaseFent();
 });
 
 fentNeedleBuy.addEventListener("click", function() {
@@ -80,8 +142,55 @@ fentPipeBuy.addEventListener("click", function() {
     fentPipe.buy();
 });
 
+fentReactorBuy.addEventListener("click", function() {
+    fentReactor.buy();
+});
+
+jewishFloydBuy.addEventListener("click", function() {
+    jewishFloyd.buy();
+});
+
+crackBuy.addEventListener("click", function() {
+    if(Fent >= 90000)
+    {
+        Fent -= 90000;
+        if(globalClickMult == 1) globalClickMult = 10;
+        else globalClickMult += 10;
+
+        crackBuy.setAttribute("disabled", "");
+        boostContainer.innerHTML = "<p>Crack boost (x10 Clicks)</p>"
+
+        window.setTimeout(function (){
+            boostContainer.innerHTML = "";
+            globalClickMult -= 10;
+            if(globalClickMult == 0) globalClickMult = 1;
+            crackBuy.removeAttribute("disabled");
+        }, 1000 * 60 * 5);
+    }
+});
 
 function update()
 {
     currentFent.innerHTML = "FentStash: " + Fent + "g";
+    fentPerClick.innerHTML = "Fent per click: " + (GameHandler.clickValue()) + "g";
+    autoFarmAmount.innerHTML = "Fent per Second: " + GameHandler.farmValue() + "g";
 }
+
+var secLoop = window.setInterval(function(){
+  Fent += GameHandler.farmValue();
+}, 1000);
+
+var gameLoop = window.setInterval(function(){
+  update();
+  if(Fent > localStorage.getItem("highscore") || 0)
+    {
+        localStorage.setItem("highscore", Fent); 
+    }
+}, 10);
+
+var hourly = window.setInterval(function(){
+    dealer.classList.remove("hidden");
+    window.setTimeout(function (){
+        dealer.classList.add("hidden");
+    }, 1000 * 60 * 2);
+}, 1000 * 60 * 60)
--- FentClicker/js/highscore.js
+++ FentClicker/js/highscore.js
@@ -1,24 +1,4 @@
-/*let fentCount = 0;
-let fentPerClick = 1;
 let highscore = localStorage.getItem("highscore") || 0;
 
-const fentDisplay = document.getElementById("currentFent");
-const fentClickButton = document.getElementById("fentClick");
-const highscoreDisplay = document.getElementById("highscore");
-highscoreDisplay.innerText = `Highscore: ${highscore}`;
-
-document.body.insertBefore(highscoreDisplay, fentDisplay.nextSibling);
-
-fentClickButton.addEventListener("click", () => {
-    fentCount += fentPerClick;
-    fentDisplay.innerText = `FentStash: ${fentCount}`;
-
-    if (fentCount > highscore) {
-        highscore = fentCount;
-        localStorage.setItem("highscore", highscore);
-        highscoreDisplay.innerText = `Highscore: ${highscore}`;
-    }
-});
-
-geht nu nd zählt immer mit 1 egal ob upgraded
-*/
+const highscoreDisplay = document.getElementById("highscoreDisplay");
+highscoreDisplay.innerHTML = `${highscore} g's of Fent`;
