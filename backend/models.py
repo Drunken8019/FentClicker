e72b43b1c8e7c0072ecea7b029f5efd91fb4749e
Lel543 <haidererpaul@gmail.com>
2025-09-09 12:45:52 +0200

    backend GET implement

---
+++ backend/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Date
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    created_at = Column(Date, index=True)
+    highscore = Column(String, index=True)
